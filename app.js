9a3f5fa5fbd3a2074dec50de1e843692f1d48540
dannibla <dannibla@outlook.com>
2019-03-20 18:08:59 +0530

    updated with pg-dynamic-crud code

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const path = require('path');
-
-var customers = require('./routes/customers'); 
-var routes = require('./routes');
+var dc = require('pg-dynamic-crud');
 var app = express();
 
-app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+dc.connect({
+    connectionString: 'postgres://postgres:postgres@localhost:5432/database',
+    crud: [{
+        table: 'customer',
+        primary: 'id',
+        route: 'customers',
+        listView: 'customer/list',
+        formView: 'customer/form'
+    }, {
+        table: 'user',
+        primary: 'id',
+        route: 'users',
+        listView: 'user/list',
+        formView: 'user/form'
+    }]
+}, function (err, result) {
+    console.log(err, result);
+});
+
+app.set('port', process.env.PORT || 4000);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.index);
-app.get('/customers', customers.list);
-app.get('/customers/add', customers.add);
-app.post('/customers/add', customers.save);
-app.get('/customers/delete/:id', customers.delete);
-app.get('/customers/edit/:id', customers.edit);
-app.post('/customers/edit/:id', customers.update);
+app.use('/', dc);
 
 app.listen(4000, function () {
     console.log('Server is running.. on Port 4000');
